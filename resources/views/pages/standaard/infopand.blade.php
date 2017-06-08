0b8084e0ee81328f6dbbf0427a163748b7b750b7
Tim <gaaramember@gmail.com>
2017-06-08 20:11:46 +0200

    Logo in de header veranderd naar svg

--- resources/views/pages/standaard/infopand.blade.php
+++ resources/views/pages/standaard/infopand.blade.php
@@ -36,7 +36,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-3 col-xs-12">
-                <a href="{{route('index')}}"><img id="headerLogo" src="../../public/assets/images/chameleon-blue-txt.png" alt="Dynamic City Logo"></a>
+                <a href="{{route('index')}}"><img id="headerLogo" src="/public/assets/images/chameleon-blue-txt.svg" alt="Dynamic City Logo"></a>
             </div>
             <nav id="headerNav" class="col-md-9 col-sm-9 col-xs-12">
              @include('menu')
--- resources/views/pages/standaard/voorwaarde.blade.php
+++ resources/views/pages/standaard/voorwaarde.blade.php
@@ -34,7 +34,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-3 col-xs-12">
-                <a href="{{route('index')}}"><img id="headerLogo" src="/public/assets/images/chameleon-blue-txt.png"
+                <a href="{{route('index')}}"><img id="headerLogo" src="/public/assets/images/chameleon-blue-txt.svg"
                                                   alt="Dynamic City Logo"></a>
             </div>
             <nav id="headerNav" class="col-md-9 col-sm-9 col-xs-12">
