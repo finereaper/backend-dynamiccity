f7f02d3b304b07121d8b19451eae5cc947df862c
Tim <gaaramember@gmail.com>
2017-06-12 13:46:49 +0200

    Huurafspraakform geupdate

--- resources/views/pages/standaard/huurafspraakForm.blade.php
+++ resources/views/pages/standaard/huurafspraakForm.blade.php
@@ -1,81 +1,125 @@
 @extends('layout')
 
 @section('header')
-        <!doctype html>
-<html lang="{{ config('app.locale') }}">
+        <!DOCTYPE html>
+<html>
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <title>Dynamic-city</title>
+    <!-- Project CSS -->
+    <link rel="stylesheet" type="text/css" href="/public/assets/styles/main.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/public/assets/bootstrap/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/assets/bootstrap/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.0.0/animate.min.css">
+    <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet'>
+    <link rel="stylesheet" href="/public/assets/liquidslider-master/css/liquid-slider.css">
+    <link rel="stylesheet" href="/public/assets/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--favicon link -->
+    <link rel="apple-touch-icon" sizes="180x180" href="public/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="public/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="public/assets/favicon/favicon-16x16.png">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Huurafspraak afwijzen - Dynamic City</title>
+    <!--favicon link -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/public/assets/favicon/manifest.json">
+    <link rel="mask-icon" href="/public/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
 </head>
+
 <body>
-<article id="menu">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container text-center">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    <img alt="Dynamic-city" src="...">
-                </a>
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="search" placeholder="Zoek een stad in de buurt">
-                    </div>
-                </form>
+
+<!-- Header -->
+<header id="headerOpmaak">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-12">
+                <a href="{{route('index')}}"><img id="headerLogo" src="/public/assets/images/chameleon-blue-txt.svg" alt="Dynamic City Logo"></a>
             </div>
+            <nav id="headerNav" class="col-md-9 col-sm-9 col-xs-12">
+            @include('menu')
         </div>
-    </nav>
-</article>
+    </div>
+</header>
 @endsection
 
 @section('main')
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8 col-xs-8 col-lg-8 text-center centerCol">
-                <form class="form-horizontal" action="" method="post">
-                    {{ csrf_field() }}
-                    <fieldset>
-                        <legend>Reden van afwijzen</legend>
-
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="gebruikerReden"></label>
-                            <div class="col-sm-10">
-                                <textarea class="form-control" id="gebruikerReden" name="gebruikerReden" rows="1" placeholder="Vul hier de reden in van het afwijzen van het verzoek."
-                                          required=""></textarea>
-
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="control-label col-sm-2" for="gebruikerSubmit"></label>
-                            <div class="text-center col-sm-10">
-                                <button type="submit" id="gebruikerSubmit" name="gebruikerSubmit"
-                                        class="btn btn-primary" aria-label="">Vertuur
-                                </button>
-
-                            </div>
-                        </div>
-
-                    </fieldset>
-                </form>
-            </div>
+    <main>
+        <div class="pandBackground">
+            <img src="/public/assets/images/venlo.png">
         </div>
-    </div>
+        <section id="introSection" style="background-image: none !important;">
+            <div class="container" style="padding: 50px 0px;">
+                <div id="pandInfoBlok" class="col-md-offset-1 col-md-10 col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10">
+                    <h2 align="center" style="color: white">Afspraak afwijzen</h2>
+                    <form action="" method="post">
+                        {{ csrf_field() }}
+                        <textarea id="afwijsText" name="gebruikerReden">
+                                </textarea>
+                        <input class="afwijsButton" name="gebruikerSubmit" type="submit" value="Verstuur Bericht">
+                    </form>
+                </div>
+            </div>
+        </section>
+    </main>
 @endsection
 
 @section('footer')
+    <footer>
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10 col-sm-12 col-xs-12">
+                <h2 class="text-center">Dynamic-city</h2>
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-12 text-center">
+                        <ul class="footerMenu">
+                            <li>Contact</li>
+                            <li><i class="adres fa fa-home"></i>Rachelsmolen 1</li>
+                            <li><i class="adres"></i>5612 MA, Eindhoven</li>
+                            <li><i class="adres fa fa-envelope"></i>projectobbi@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div class="mobileFooterPadding col-md-4 col-sm-4 col-xs-12 text-center">
+                        <ul class="footerMenu">
+                            <li>Social media</li>
+                            <li><a href="https://www.facebook.com/DynamicCityNL" target="_blank"><i
+                                            class="social fa fa-facebook"></i></a></li>
+                            <li><a href="https://twitter.com/dynamic_cityNL" target="_blank"><i
+                                            class="social fa fa-twitter"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/dynamic-city-aa08a7144/" target="_blank"><i
+                                            class="social fa fa-linkedin"></i></a></li>
+                            <li><a href="https://nl.pinterest.com/dynamiccity0274/" target="_blank"><i
+                                            class="social fa fa-pinterest"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 text-center">
+                        <ul class="footerMenu">
+                            <li>Menu</li>
+                            <li><a href="http://dynamic-city.nl/panden/overzicht">Ruimtes</a></li>
+                            <li><a href="http://dynamic-city.nl/verhuurder/registratie">Word verhuurder</a></li>
+                            <li><a href="http://dynamic-city.nl/huurder/registratie">Aanmelden</a></li>
+                            <li><a href="http://dynamic-city.nl/huurder/inloggen">Inloggen</a></li>
+                            <li><a href="http://dynamic-city.nl/info">Info</a></li>
+                            <li><a href="http://dynamic-city.nl/overons">Over ons</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js"></script>
+                <script src="/public/assets/liquidslider-master/js/jquery.liquid-slider.min.js"></script>
+                @include('analytics')
+                <script src="/public/js/main.js"></script>
+                <script src="/public/assets/js/schnitzelMenu.js"></script>
+                <script>
+                    $('#main-slider').liquidSlider();
+
+                </script>
+            </div>
+        </div>
+    </footer>
 </body>
-<script src="https://code.jquery.com/jquery-2.2.4.min.js"
-        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous"></script>
-<script type="text/javascript"
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBedVnTXvMSMhIl6cKmuKElCTbo2oQ7rpI&libraries=places&language=nl&region=NL"></script>
-<script src="/js/google.js"></script>
+
 </html>
 @endsection
